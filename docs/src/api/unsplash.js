6415994ccaefb724f1f6f6c4d9d79e52e0d6f7b7
klau5 <45886598+klau5@users.noreply.github.com>
2019-06-27 04:04:47 +0530

    -

--- docs/src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-	baseURL: "https://api.unsplash.com",
-	headers: {
-		Authorization:
-			"Client-ID d5b276dc9d634a2088f008322b145f149e1116b325e39d4547315c347f56425c"
-	}
-});
